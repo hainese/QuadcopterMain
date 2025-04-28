cb8d7d2281420f26baf108a0a11e0a8b1a40c31e
dehobbit100 <fordmi@oregonstate.edu>
2025-04-28 10:00:33 -0700

    added comments to pidController.cpp to clarify each section of the code for testing and debugging

--- ESP32Code/src/pidController.cpp
+++ ESP32Code/src/pidController.cpp
@@ -6,14 +6,18 @@
 // PID controller code derived from the following video:
 // https://www.youtube.com/watch?v=jY6bBcMtseY
 
+// time between each iteration
+// likely needs to be changed based on the rest of the system
 float ts = 0.01;
 
 // PID controller values
+// change these values to tune the PID controller based on the system
 float p = 0.1; // proportional gain
 float i = 0.01; // integral gain
 float d = 0.01; // derivative gain
 
 // calculate desired rates for roll, pitch, and yaw
+// may need to change
 float desiredRate(float inputValue){
     //float output = 0.15 * (inputValue-1500);
     //float output = inputValue - 127.5; // map input value from 0-255 to -127.5 to 127.5
@@ -23,18 +27,20 @@ float desiredRate(float inputValue){
 
 // calculate error values for roll, pitch, and yaw
 float errorValue(float desiredRate, float rate) {
-    float output = desiredRate - rate;
-    return output;
+    return desiredRate - rate;
 }
 
 // calculate PID input for roll, pitch, and yaw
 float calculatePIDinput(float p, float i, float d, float currError, float *prevError, float *prevI){
+    // PID control equation:
     float output = p * currError + *prevI + i * (currError + *prevError) * ts / 2 + d * (currError - *prevError) / ts; 
-    *prevError = currError;
-    *prevI = i;
+    *prevError = currError; // update previous error
+    *prevI = i; // update previous integral value
     return output;
 }
 
+// limits output to 0-100% duty cycle
+// likely needs to be changed if the output is consistently above 100% or below 0%
 float saturateDutyCycle(float value){
     if(value > 100){
         return 100;
@@ -47,40 +53,36 @@ float saturateDutyCycle(float value){
 
 // Duty Cycle for motor 1
 float calculateDutyCycle1(float throttle, float roll, float pitch, float yaw){
-    float output = saturateDutyCycle(throttle - pitch - roll - yaw);
-    return output;
+    return saturateDutyCycle(throttle - pitch - roll - yaw);
 }
 
 // Duty Cycle for motor 2
 float calculateDutyCycle2(float throttle, float roll, float pitch, float yaw){
-    float output = saturateDutyCycle(throttle + pitch - roll + yaw);
-    return output;
+    return saturateDutyCycle(throttle + pitch - roll + yaw);
 }
 
 // Duty Cycle for motor 3
 float calculateDutyCycle3(float throttle, float roll, float pitch, float yaw){
-    float output = saturateDutyCycle(throttle + pitch + roll - yaw);
-    return output;
+    return saturateDutyCycle(throttle + pitch + roll - yaw);
 }
 
 // Duty Cycle for motor 4
 float calculateDutyCycle4(float throttle, float roll, float pitch, float yaw){
-    float output = saturateDutyCycle(throttle - pitch + roll + yaw);
-    return output;
+    return saturateDutyCycle(throttle - pitch + roll + yaw);
 }
 
 // PID control block
 void pidControl(float *prevRollError, float *prevRollI,
                 float *prevPitchError, float *prevPitchI,
                 float *prevYawError, float *prevYawI,
-                float rollInput, 
-                float pitchInput, 
-                float yawInput, 
-                float throttleInput, 
-                float rollRate, 
-                float pitchRate, 
-                float yawRate,
-                float *dutyCycles
+                float rollInput, // user input for roll
+                float pitchInput, // user input for pitch
+                float yawInput, // user input for yaw
+                float throttleInput, // user input for throttle
+                float rollRate, // gyro input for roll
+                float pitchRate, // gyro input for pitch
+                float yawRate, // gyro input for yaw
+                float *dutyCycles // array to hold duty cycles for each motor
             ) {
 
     // desired rates for roll, pitch, and yaw
